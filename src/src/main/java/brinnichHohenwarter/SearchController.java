e47377bad6ae148ce129e901b049da2cda305f2c
Selina Brinnich <sbrinnich@student.tgm.ac.at>
2015-12-18 12:46:54 +0100

    Edited

--- src/src/main/java/brinnichHohenwarter/SearchController.java
+++ src/src/main/java/brinnichHohenwarter/SearchController.java
@@ -1,25 +1,33 @@
 package brinnichHohenwarter;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @RestController
 public class SearchController {
 
+    @RequestMapping(value = "/testy", method= RequestMethod.GET, produces = "text/html")
+    public String read(){
+        return "index";
+    }
+
+    /**
     @RequestMapping("/add")
     public boolean add(@RequestParam(value="email") String email, @RequestParam(value="bio") String bio){
         return true;
     }
 
     @RequestMapping("/edit")
-    public Search edit(@RequestParam(value="email") String email, @RequestParam(value="bio") String bio){
-        return new Search("asd","asd");
+    public boolean edit(@RequestParam(value="email") String email, @RequestParam(value="bio") String bio){
+        return true;
     }
 
     @RequestMapping("/delete")
     public boolean delete(@RequestParam(value="email") String email){
         return true;
     }
+    */
 }
